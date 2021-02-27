f669a7129e51adb3866f8c8ebddb518d516d2ef0
juan_fuentes99@hotmail.com <MinionsxD14%>
2021-02-26 20:50:59 -0600

    add images

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.label import MDLabel
+from kivymd.uix.list import MDList,ThreeLineAvatarListItem,ImageLeftWidget
+from kivy.uix.scrollview import ScrollView
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class PersonaProjectApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = 'Dark';
+        screen = Screen()
+        scroll = ScrollView()
+        listView = MDList()
+        scroll.add_widget(listView)
+        for i in range (1,6):
+            img = ImageLeftWidget(source = str(i) + ".jpg")
+            items = ThreeLineAvatarListItem(text='Persona ' + str(i),secondary_text = 'Persona', tertiary_text='Test')
+            items.add_widget(img)
+            listView.add_widget(items)
+        screen.add_widget(scroll)
+        return screen
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+PersonaProjectApp().run()
